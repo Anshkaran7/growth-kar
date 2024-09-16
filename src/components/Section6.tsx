d4a11f481c985506fbde46708f01e7bc788564dc
anshkaran7 <karanchandravanshi217@gmail.com>
2024-09-17 01:19:36 +0530

    ok

--- src/components/Section6.tsx
+++ src/components/Section6.tsx
@@ -41,13 +41,13 @@ export default function Section6({ style }: Section6Props) {
 
   return (
     <div
-      className={`relative flex items-center justify-center min-h-[60vh] p-8 bg-black text-white ${style}`}
+      className={`relative flex items-center justify-center min-h-[60vh] md:p-8 bg-black text-white ${style}`}
     >
       {/* Background Image Container */}
       <div className="relative w-full h-full flex items-center justify-center">
         {/* Background Image */}
         <div
-          className="absolute inset-0 overflow-hidden"
+          className="absolute md:flex hidden inset-0 overflow-hidden"
           style={{
             backgroundImage: "url('/assets/bg.png')",
             backgroundSize: "50% 50%",
@@ -58,7 +58,7 @@ export default function Section6({ style }: Section6Props) {
         />
 
         {/* Help Section Image on Top */}
-        <div className="absolute -translate-x-12 -translate-y-1 w-1/2 h-auto">
+        <div className="absolute md:flex hidden -translate-x-12 -translate-y-1 w-1/2 h-auto">
           <Image
             src={"/assets/helpSection.png"}
             alt={"Help Section"}
@@ -67,12 +67,38 @@ export default function Section6({ style }: Section6Props) {
             className="object-contain h-[370px] w-[370px]"
           />
         </div>
+        {/* <div className="md:hidden flex w-[70%]">
+          <Image
+            src={"/assets/helpSection.png"}
+            alt={"Help Section"}
+            width={1000}
+            height={1000}
+            className="object-contain h-[370px] w-[370px]"
+          />
+        </div> */}
 
-        {/* Overlay Content with Slide Up and Fade Out Effect */}
         {sections.map((section, index) => (
           <div
             key={index}
-            className={`absolute right-10 w-1/2 items-end space-x-8 z-10 ${
+            className={`absolute md:hidden flex  w-[90%] md:w-1/2 items-end space-x-8 z-10 ${
+              activeIndex === index ? animationClass : "opacity-0"
+            }`}
+            style={{
+              transition: "opacity 0.5s ease-in-out",
+            }}
+          >
+            <div className="w-full max-w-[350px] p-4"> {/* Added max-width and padding */}
+              <h2 className="text-3xl font-bold mb-4">{section.title}</h2>
+              <p className="text-lg text-[#575757] w-full break-words">
+                {section.content}
+              </p>
+            </div>
+          </div>
+        ))}
+        {sections.map((section, index) => (
+          <div
+            key={index}
+            className={`absolute md:flex hidden right-10 w-1/2 items-end space-x-8 z-10 ${
               activeIndex === index ? animationClass : "opacity-0"
             }`}
             style={{
